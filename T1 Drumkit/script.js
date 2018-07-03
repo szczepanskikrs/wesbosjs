d4017f2721eb4bd105d6c547432c8ffea0940d79
Krzysztof_Szczepansk <krzysztof_szczepanski@epam.com>
2018-07-03 08:21:15 +0200

    Drum Kit finished

---
+++ T1 Drumkit/script.js
@@ -0,0 +1,28 @@
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') { return }
+    console.log("DUPA")
+    this.classList.remove('playing')
+}
+
+function playAudio(audio, key) {
+    if (!audio) { return }
+    else {
+        audio.currentTime = 0
+        audio.play()
+        key.classList.add('playing')
+    }
+}
+
+function setKeyEventListeners(keys) {
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+}
+
+function playSound(event) {
+    const audio = this.document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = this.document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    const keys = this.document.querySelectorAll(`.key`)
+    setKeyEventListeners(keys)
+    playAudio(audio, key)
+}
+
+window.addEventListener('keydown', playSound)
